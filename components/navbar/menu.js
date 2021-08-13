235f62838c96f618475a0893da8c5ad5286417b6
Melanie Bezot-Trottetfr <melanie@bigmouthfrog.fr>
2021-08-13 10:26:11 +0200

    fixed mobile menu

--- components/navbar/menu.js
+++ components/navbar/menu.js
@@ -2,20 +2,14 @@ import Link from 'next/link'
 import React, { useState, useEffect } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
-
 const Menu = () => {
 
-        const [click, setClick] = useState(false)
-        const [navbar, setNavbar] = useState(false)
-        // TOGGLE
-        const handleClick = () => {
-            setClick(!click)
-        }
+        const [click, setClick] = useState(false);
+        const handleClick = () => setClick(!click);
+        const closeMobileMenu = () => setClick(false);
     
-        const closeMobileMenu = () => {
-            setClick(false)
-        }
         // change BG color
+        const [navbar, setNavbar] = useState(false)
         const changeBackground = () => {
             if(window.scrollY >=100)
             {
@@ -27,8 +21,6 @@ const Menu = () => {
         if (typeof window !== "undefined") {
             window.addEventListener('scroll', changeBackground);
           }
-        
-
 
     return (
         <>
@@ -45,30 +37,25 @@ const Menu = () => {
                             {click ? <FaTimes /> : <FaBars />}
                         </div>
                         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                            <li className='nav-item'>
-                                <Link onClick={closeMobileMenu} 
-                                className='nav-link' href='/'><a>Accueil</a></Link>
+                            <li className='nav-item' onClick={closeMobileMenu}>
+                                <Link className='nav-link' href='/'><a>Accueil</a></Link>
                             </li>
-                            <li className='nav-item'>
-                                <Link onClick={closeMobileMenu} 
-                                className='nav-link' href='/strategie_digitale'><a>Stratégie digitale</a></Link>
+                            <li className='nav-item'onClick={closeMobileMenu}>
+                                <Link className='nav-link' href='/strategie_digitale'><a>Stratégie digitale</a></Link>
                             </li>
-                            <li className='nav-item'>
-                                <Link onClick={closeMobileMenu} 
-                                className='nav-link' href='/marketing_digital'><a>Marketing Digital</a></Link>
+                            <li className='nav-item'onClick={closeMobileMenu}>
+                                <Link className='nav-link' href='/marketing_digital'><a>Marketing Digital</a></Link>
                             </li>
-                            <li className='nav-item'>
-                                <Link onClick={closeMobileMenu} 
-                                className='nav-link' href='/web_design'><a>Web Design</a></Link>
+                            <li className='nav-item'onClick={closeMobileMenu}>
+                                <Link className='nav-link' href='/web_design'><a>Web Design</a></Link>
                             </li>
-                            <li className='nav-item'>
-                                <Link onClick={closeMobileMenu} 
-                                className='nav-link' href='/a_propos'><a>A Propos</a></Link>
+                            <li className='nav-item'onClick={closeMobileMenu}>
+                                <Link className='nav-link' href='/a_propos'><a>A Propos</a></Link>
                             </li>
 
                             <div className="navButton right">
-                            <button className="menuButton">
-                                <Link onClick={closeMobileMenu} href="#contact"><a>
+                            <button className="menuButton"  onClick={handleClick}>
+                                <Link  href="#contact"><a>
                                 Contact</a>
                                 </Link>
                             </button>
